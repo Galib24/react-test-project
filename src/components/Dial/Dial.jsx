fde14dbd835cfcae2af612e8e5537afb7ab571d6
SQLAdmin <abuyeahia24@gmail.com>
2023-03-24 03:25:20 +0600

    added nested components

--- src/components/Dial/Dial.jsx
+++ src/components/Dial/Dial.jsx
@@ -1,10 +1,12 @@
 import React from 'react';
+import Knob from '../Knob/Knob';
 
 const Dial = (props) => {
     return (
         <div style={{border:'2px solid purple', margin: '20px'}}>
             <h3>Your steps count</h3>
             <p>Your steps count: {props.steps}</p>
+            <Knob steps={props.steps}></Knob>
         </div>
     );
 };
--- src/components/Knob/Knob.jsx
+++ src/components/Knob/Knob.jsx
@@ -1,9 +1,10 @@
 import React from 'react';
 
-const Knob = () => {
+const Knob = (props) => {
     return (
-        <div>
-            
+        <div style={{border: '2px solid green', margin: '20px'}}>
+            <h5>This is knob component</h5>
+            <p>Steps here is: {props.steps}</p>
         </div>
     );
 };
